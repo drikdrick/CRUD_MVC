8409155152e3f771b5de0c2faa167eff2bf5fdaf
Fedrick Sulaiman Siagian <superiorsiagian@gmail.com>
2020-07-15 01:57:03 +0700

    Initial commit

---
+++ Inventory/Controllers/BarangController.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Inventory.Models;
+
+namespace Inventory.Controllers
+{
+    public class BarangController : Controller
+    {
+        // GET: Barang
+        public ActionResult Index()
+        {
+            using (inventoryEntities dbModel = new inventoryEntities())
+            {
+                return View(dbModel.barangs.ToList());
+            }
+        }
+
+        // GET: Barang/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Barang/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Barang/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Barang/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Barang/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Barang/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Barang/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
